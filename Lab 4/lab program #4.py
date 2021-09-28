cf06bbb73062b1d51f8ab6cf6f61cdfe8e03ece9
Ngbailey <91026635+Ngbailey@users.noreply.github.com>
2021-09-28 15:24:21 -0500

    Update lab program #4.py

--- Lab 4/lab program #4.py
+++ Lab 4/lab program #4.py
@@ -17,7 +17,7 @@ def play_again(): #This function allows the User to "play again".
             return False
         else:
             playAgain = input('Please input correct inputs only.  Do you want to play again? Y or YES to continue, N or NO to quit\n')
-chips = 200
+
 def get_wager(bank : chips): #function to return an integer
     chips = int(input('How many chips would you like to wager?\n'))
     checkerchips = -1
@@ -55,3 +55,12 @@ def get_bank():
             bank = int(input('How many chips do you want to start with?\n'))
         else:
             break
+def get_payout(wager, matches):
+    if matches == 3:
+        payout = wager*10 - wager
+    if matches == 2:
+        payout = wager*3 - wager
+    else:
+        payout = -1*wager
+    return payout
+
