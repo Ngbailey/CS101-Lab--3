b53c1a9a137175418221752c442812d77000d764
Ngbailey <91026635+Ngbailey@users.noreply.github.com>
2021-09-29 21:08:03 -0500

    Update lab program #4.py

--- Lab 4/lab program #4.py
+++ Lab 4/lab program #4.py
@@ -4,41 +4,36 @@
 #September 28th 2021
 import random
 checker = -1
-def play_again(): #This function allows the User to "play again".
-    playAgain = input('Do you want to play again? Y or YES to continue, N or NO to quit\n')
+#Play again function#
+def play_again():
     cheppers = -1
     while cheppers == -1:
-        if playAgain == 'Y':
+        playAgain = input('Do you want to play again? Y or YES to continue, N or NO to quit==>')
+        if playAgain == 'Y' or playAgain == 'YES':
             cheppers = 200
             return True
-        if playAgain == 'YES':
-            cheppers = 200
-            return True
-        elif playAgain == 'N':
-            cheppers = 200
-            return False
-        elif playAgain == 'NO':
-            cheppers = 200
+        elif playAgain == 'N' or playAgain == 'NO':
+            cheppers = 0
             return False
         else:
-            playAgain = input('Please input correct inputs only.  Do you want to play again? Y or YES to continue, N or NO to quit\n')
-
-
-chips = 200
-bank = 100
-def get_wager(bank , chips): #function to return an integer
-    chips = int(input('How many chips would you like to wager?'))
+            print('Please input Y/YES or N/NO only.')
+    
+#get wager function#
+def get_wager(bank:int)->int:
+    chips = int(input('How many chips would you like to wager?==>'))
     checkerchips = -1
     while checkerchips == -1:
         if chips <= 0:
             print('Please input a value greater than 0')
-            chips = int(input('How many chips would you like to wager?\n'))
+            chips = int(input('How many chips would you like to wager?==>'))
         if chips > bank:
             print('you cannot wager more than you have.')
-            chips = int(input('How many chips would you like to wager?\n'))
+            chips = int(input('How many chips would you like to wager?==>'))
         else:
+            chips = 1*chips
             checkerchips = 200
-
+            return chips
+#Slot result function#
 def get_slot_results():
     x = random.randint(1,10)
     y = random.randint(1,10)
@@ -49,56 +44,61 @@ def get_slot_results():
         print(i,end=' ')
     print()
     return slotresult
-
-
-def get_matches(reela, reelb, reelc):
-    if reela == reelb == reelc:
-        return 3
-    elif reela == reelb or reela == reelc or reelb == reelc:
-        return 2
-    else:
-        return 0
-
+#matching function#
+def get_matches(x,y,z):
+        if x == y == z:
+            matches = 3
+        elif x == y or x == z or y == z:
+            matches = 2
+        else:
+            matches = 0
+        return matches
+#Starting chips function#
 def get_bank():
-    bank = int(input('How many chips do you want to start with?'))
-    while bank <=0 or bank >= 101:
+    checkers =-1
+    bank = int(input('How many chips do you want to start with?==>'))
+    while checkers == -1:
         if bank <= 0:
             print('Too low a value, you can only choose 1 -100 chips')
-            bank = int(input('How many chips do you want to start with?\n'))
+            bank = int(input('How many chips do you want to start with?==>'))
         if bank >= 101:
             print('Too high a value, you can only choose 1 -100 chips')
-            bank = int(input('How many chips do you want to start with?\n'))
+            bank = int(input('How many chips do you want to start with?==>'))
         else:
-            break
-
-def get_payout(wager, matches):
-    if matches == 3:
-        wager += wager*10 - wager
-    if matches == 2:
-        wager += wager*3 - wager
-    else:
-        wager -= wager
-    return wager
-
-get_bank()
-while checker == -1:
-    get_wager(bank , chips)
-    get_slot_results()
-    slotresult = get_slot_results
-    for i in slotresult:
-        slotresult.append(i)
-    x = slotresult[0]
-    y = slotresult[1]
-    z = slotresult[2]
-    get_matches(x,y,z)
-    get_payout(get_wager,get_matches(x,y,z))
-    play_again()
-    checkers = -1
-    while checkers == -1:
-        if play_again() is True:
-            break
-        else:
+            bank = 1*bank
             checkers = 200
-            checker = 200
-            exit
+            return bank
+#payout function#
+def get_payout(chips,matches):
+    if matches == 3:
+        print('JACKPOT you won {} chips\n'.format(chips*10-chips))
+        chips += chips*9 - chips
+        return chips
+    if matches == 2:
+        print('you won {} chips\n'.format(chips*3-chips))
+        chips += chips*2 - chips
+        return chips
+    else:
+        print('Sorry you lost {} chips\n'.format(chips))
+        chips -= 2*chips
+        return chips
+    
 
+
+bank = get_bank()
+while checker ==-1:
+    chips = get_wager(bank)
+    x,y,z = get_slot_results()
+    matches = get_matches(x,y,z)
+    if matches >= 2:
+        bank += get_payout(chips,matches)
+    else:
+        bank += get_payout(chips,matches)
+    print('You have {} chips left\n'.format(bank))
+    play_again()
+    if play_again == True:
+        checker = -1
+    else:
+        checker = 200
+        exit
+        
